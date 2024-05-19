2e4ebbb476742173fa14052d047e586cd3eb6ef6
Muhammad Daffa Amirul Wachid <daffanaw@yahoo.co.id>
2024-05-19 21:41:37 +0700

    Update Login.php

--- application/controllers/Login.php
+++ application/controllers/Login.php
@@ -22,11 +22,11 @@ class Login extends CI_Controller {
 			if (!empty($result) && password_verify($data['pass'], $result->pass)) {
 				$sessionData = array('user' => $result->user, 'id' => $result->kode_user, 'level' => $result->level);
 				$this->session->set_userdata($sessionData);
-				return redirect('/dashboard');
+				return redirect('/Dashboard');
 			} 
 			$this->session->set_flashdata('message', 'Username / Password anda salah');
 		}
 
-		$this->load->view('login');
+		$this->load->view('pages/login');
 	}
 }
